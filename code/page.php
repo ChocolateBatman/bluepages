815670dca9c7eab10a0351f327004284fb59a76c
yusufcodes <chocolatebatman99@gmail.com>
2019-01-30 16:14:50 +0000

    Added a breadcrumb navigation area to direct users from a child page back to the parent page

--- code/page.php
+++ code/page.php
@@ -1,18 +1,33 @@
 <?php
-get_header();
-// The Wordpress Loop
-while(have_posts()) // While there is a post
+get_header(); // Displaying the header - navigation and title
+
+while (have_posts()) // While there is a 'post' - (a page to display)
 {
-    the_post(); ?> <!-- Get information about the next post -->
-    <!-- Closing PHP --> 
+    the_post(); ?> <!-- Get information about the next page -->
 
-    <h1><?php the_title(); ?></h1>
+    <h1><?php the_title(); ?></h1> <!-- Outputting the title of the current page -->
 
+    <!--  *** Start: Breadcrumb navigation area ***
+    to display 'previous' page as a link for the user -->
+    <?php
+
+    // get_the_ID() -> Retrieves the ID of the CURRENT page
+    // wp_get_post_parent_id() -> Retrieves the ID of the Parent page, of the specified page
+    $theParent = wp_get_post_parent_id(get_the_ID());
+    
+    /* If a parent ID exists, the variable 'theParent' will hold True. That means the HTML content will display - only on the children pages. */
+    if ($theParent) 
+    { ?>
+        <a href="<?php echo get_permalink($theParent); ?>">Back to: <?php echo get_the_title($theParent);?> </a><p>Current page: <?php the_title(); ?></p>
+
+    <?php } ?> <!-- End If loop -->
+
+    <!-- *** End: Breadcrumb navigation area -->
+    
+    <!-- Displaying the contents of the current page -->
     <?php the_content(); ?>
 
-
-<!-- Opening PHP --><?php }
-
+<!-- Ending the while loop and outputting the footer of the website -->
+<?php }
 get_footer();
-
 ?>
