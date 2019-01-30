0e2bf1fe20beeaea85e8bc25484330335ba460b8
yusufcodes <chocolatebatman99@gmail.com>
2019-01-30 17:29:00 +0000

    Added a list of links for each category

--- code/page.php
+++ code/page.php
@@ -21,8 +21,44 @@ while (have_posts()) // While there is a 'post' - (a page to display)
         <a href="<?php echo get_permalink($theParent); ?>">Back to: <?php echo get_the_title($theParent);?> </a><p>Current page: <?php the_title(); ?></p>
 
     <?php } ?> <!-- End If loop -->
+    <!-- *** End: Breadcrumb navigation area *** -->
 
-    <!-- *** End: Breadcrumb navigation area -->
+
+    <!--  *** START: List of links ***
+    To display 'child' pages of a parent page -->
+
+    <!-- Display menu IF: on a child page, OR on a parent page
+    This will stop the list of links from displaying if we are on a page that is neither a child page or a parent page with children. -->
+    <?php
+    // Retrieves a set of pages for the current page
+    $checkIfParentPage = get_pages( array (
+        'child_of' => get_the_ID()
+    ));
+    
+    if ($theParent or $checkIfParentPage) { ?>
+    <div id="page-links">
+
+        <?php 
+        // If parent page exists, set ID to the parent, otherwise set the ID to the current child page.
+        if ($theParent)
+        $pageID = $theParent;
+
+        else
+        $pageID = get_the_ID();
+        ?>
+
+        <h2><a href="<?php echo get_permalink($theParent)?>">Categories - <?php echo get_the_title($theParent); ?></a></h2> <!-- Displays heading with the name of the category, also a link to the parent category -->
+
+        <?php
+        // Displays the list of pages within a category
+        wp_list_pages( array(
+            'title_li'=> NULL, // Hides the default heading given by the function
+            'child_of' => $pageID, // Gets links to corresponding page 
+            'sort_column' => 'menu_order' // Sets ordering of links based on 'Order' attribute in WP panel
+        ));?>
+    </div>
+    <?php } ?>
+    <!--  *** END: List of links *** -->
     
     <!-- Displaying the contents of the current page -->
     <?php the_content(); ?>
