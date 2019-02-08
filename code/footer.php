c22f01adb861c0329f253fb134f14a6fbb68ba5b
yusufcodes <chocolatebatman99@gmail.com>
2019-02-08 14:25:46 +0000

    Added content to the footer

--- code/footer.php
+++ code/footer.php
@@ -1,5 +1,12 @@
 <footer>
-    <h2>Footer</h2>
+    <p>© <?php echo date('Y');?> National Flood Forum: a Company Limited by Guarantee. Registered in England and Wales No. 4349401.</p>
+
+    <p>Registered Office: Old Snuff Mill Warehouse, Park Lane, Bewdley, Worcestershire, DY12 2EL. VAT Registered No: 786 2681 83</p>
+
+    <p>The National Flood Forum is a Registered Charity No. 1121642</p>
+
+    <p>Site design by MMU Blue Pages Project 2018-19</p>
+    
 </footer>
 
 
